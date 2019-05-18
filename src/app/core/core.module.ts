ef8011fbabeac8ea605a96e7e2b81c377df09f32
BNPrashanth <bnprashanth256@gmail.com>
2019-05-18 23:09:50 +0530

    header, footer and login form skeleton added

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
+import { MatToolbarModule, MatIconModule, MatButtonModule, MatFormField, MatFormFieldModule, MatInputModule } from '@angular/material';
 
 import { SharedModule } from '../shared/shared.module';
 import { AppRoutingModule } from '../app-routing.module';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     WelcomeComponent,
-    LoginComponent
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     SharedModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   exports: [],
   providers: []
